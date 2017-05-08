8af90f41d86ab17c8e72918fc0c3184f2bb3dc69
aguscarloni <acarloni@itba.edu.ar>
2017-05-08 13:16:53 -0300

    Pruebita

---
+++ prueba.java
@@ -0,0 +1,8 @@
+public class prueba {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+      int b;
+      int c;
+   }
+}
